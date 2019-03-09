be8b370b59c73dc37003cde139bb16cbe3056ba1
JoeMulick <joemulick@gmail.com>
2019-03-09 00:49:29 -0800

    index and mainlandingpagesection setup correctly

--- components/MainLandingSection.js
+++ components/MainLandingSection.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 
 const styles = theme => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
+    }, 
+    mainGrid: {
+        minHeight: '100vmin',
+        width: 'auto',
+        background:'url(https://res.cloudinary.com/ddsihrmda/image/upload/v1548226609/carlos-muza-84523-unsplash_kcw88s.jpg) no-repeat fixed',
+        position: 'relative',
+        webkitBackgroundSize: 'cover',
+        mozBackgroundSize: 'cover',
+        oBackgroundSize: 'cover',
+        backgroundSize: 'cover',
     },
   });
   
@@ -15,9 +26,11 @@ const styles = theme => ({
       const { classes } = this.props;
   
       return (
-          <div className={classes.root}>
-            
-        </div> 
+        <React.Fragment>
+          <Grid className={classes.mainGrid} container>
+
+          </Grid>
+        </React.Fragment>
       );
     }
   }
--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,11 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 // import Typography from '@material-ui/core/Typography';
 // import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import MainLandingSection from '../components/MainLandingSection';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-  },
-
-  mainGrid: {
-    minHeight: '100vmin',
-    width: 'auto',
-    background:'url(https://res.cloudinary.com/ddsihrmda/image/upload/v1548226609/carlos-muza-84523-unsplash_kcw88s.jpg) no-repeat fixed',
-    position: 'relative',
-    webkitBackgroundSize: 'cover',
-    mozBackgroundSize: 'cover',
-    oBackgroundSize: 'cover',
-    backgroundSize: 'cover',
-
   },
 
 });
@@ -48,11 +36,9 @@ class Index extends React.Component {
     const { classes } = this.props;
 
     return (
-      <React.Fragment>
-          <Grid className={classes.mainGrid} container>
-
-          </Grid>
-      </React.Fragment>
+      <div className={classes.root}>
+        <MainLandingSection />
+      </div>
     );
   }
 }
