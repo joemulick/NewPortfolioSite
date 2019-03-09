f0129dc6bb8f638c9ef4f65d71420f45e4018fc1
JoeMulick <joemulick@gmail.com>
2019-03-09 00:24:49 -0800

    starting costrcution of index,js

---
+++ components/MainLandingSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+    root: {
+      flexGrow: 1,
+    },
+  });
+  
+  class MainLandingSection extends React.Component {
+  
+    render() {
+      const { classes } = this.props;
+  
+      return (
+          <div className={classes.root}>
+            
+        </div> 
+      );
+    }
+  }
+  
+  
+  
+  MainLandingSection.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(MainLandingSection);
